39b42a6830446ef85b40c8a13b1bfe0b3eb0cb27
davidnachmias <123abadodi@gmail.com>
2025-07-13 19:07:20 +0300

    change styling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,10 @@
 export default function Logo() {
   return (
     <a href="." className="logo">
-      <img
-        src="https://bytegrad.com/course-assets/js/2/logo.svg"
-        alt="Logo"
-        className="logo__img"
-      />
+      <div className="logo__text">
+        <span className="logo__primary">Dev</span>
+        <span className="logo__secondary">Match</span>
+      </div>
     </a>
   );
 }
